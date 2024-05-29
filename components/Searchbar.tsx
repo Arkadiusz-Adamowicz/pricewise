c55aabe95b5592504a5eb12a0ad9298db38a2a5e
Arek <arek.adamowicz81@gmail.com>
2024-05-29 21:14:26 +0200

    update

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,14 +1,46 @@
 'use client';
 
-import React from 'react';
+// https://www.youtube.com/watch?v=lh9XVGv6BHs&ab_channel=JavaScriptMastery
+// 1"03"04
+
+import { FormEvent, useState } from 'react';
+
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    if (
+      hostname.includes('amazon.com') ||
+      hostname.includes('amazon') ||
+      hostname.endsWith('amazon')
+    ) {
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
+  return false;
+};
 
 const Searchbar = () => {
-  const handleSubmit = () => {};
+  const [searchPrompt, setSearchPrompt] = useState('');
+  console.log(searchPrompt);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
+
+    alert(isValidLink ? 'Valid link' : 'Invalid link');
+  };
 
   return (
     <form className='flex flex-wrap gap-4 mt-12' onSubmit={handleSubmit}>
       <input
         type='text'
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
         placeholder='Enter product link'
         className='searchbar-input'
       />
