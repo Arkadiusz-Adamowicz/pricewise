a964df9c122bf69600f8fa9388927b2fee074190
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2024-06-04 23:44:39 +0200

    update

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,10 +1,79 @@
-import React from 'react';
+'use client';
+
+import {
+  Button,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  Transition,
+  TransitionChild,
+} from '@headlessui/react';
+import Image from 'next/image';
+import { Fragment, useState } from 'react';
 
 const Modal = () => {
-  return <div>Modal</div>;
+  let [isOpen, setIsOpen] = useState(true);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => setIsOpen(false);
+
+  return (
+    <>
+      <button type='button' className='btn' onClick={openModal}>
+        Track
+      </button>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as='div' onClose={closeModal} className='dialog-container'>
+          <div className='min-h-screen px-4 text-center'>
+            <TransitionChild
+              as={Fragment}
+              enter='ease-out duration-300'
+              enterFrom='opacity-0'
+              enterTo='opacity-100'
+              leave='ease-in duration-200'
+              leaveFrom='opacity-100'
+              leaveTo='opacity-0'
+            >
+              <DialogPanel className='fixed, inset-0' />
+            </TransitionChild>
+            <span className='inline-block h-screen align-middle aria-hidden:' />
+
+            <TransitionChild
+              as={Fragment}
+              enter='ease-out duration-300'
+              enterFrom='opacity-0 scale-95'
+              enterTo='opacity-100 scale-100'
+              leave='ease-in duration-200'
+              leaveFrom='opacity-100 scale-100'
+              leaveTo='opacity-0 scale-95'
+            >
+              <div className='dialog-content'>
+                <div className='flex flex-col'>
+                  <div className='flex justify-between'>
+                    <div className='p-3 border border-gray-200 rounded-10'>
+                      <Image
+                        src='/assets/icons/logo.svg'
+                        alt='logo'
+                        width={28}
+                        height={28}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </TransitionChild>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
 };
 
 export default Modal;
 
 // https://www.youtube.com/watch?v=lh9XVGv6BHs&t=8613s
-// 2:57
+// 3:05:40
--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -5,6 +5,7 @@ import Product from '../models/product.model';
 import { connectToDB } from '../mongoose';
 import { scrapeAmazonProduct } from '../scraper';
 import { getAveragePrice, getHighestPrice, getLowestPrice } from '../utils';
+import { User } from '@/types';
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
