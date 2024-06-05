9c6d4ed6caadeb8d5e69658ea2241d4457a1c5f0
Arek <arek.adamowicz81@gmail.com>
2024-06-05 09:48:10 +0200

    update

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,10 +1,8 @@
 'use client';
 
 import {
-  Button,
   Dialog,
   DialogPanel,
-  DialogTitle,
   Transition,
   TransitionChild,
 } from '@headlessui/react';
@@ -12,11 +10,13 @@ import Image from 'next/image';
 import { Fragment, useState } from 'react';
 
 const Modal = () => {
-  let [isOpen, setIsOpen] = useState(true);
+  let [isOpen, setIsOpen] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [email, setEmail] = useState('');
 
-  const openModal = () => {
-    setIsOpen(true);
-  };
+  const handleSubmit = () => {};
+
+  const openModal = () => setIsOpen(true);
 
   const closeModal = () => setIsOpen(false);
 
@@ -26,11 +26,10 @@ const Modal = () => {
         Track
       </button>
 
-      <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as='div' onClose={closeModal} className='dialog-container'>
+      <Transition appear show={isOpen}>
+        <Dialog as='div' onClose={() => {}} className='dialog-container'>
           <div className='min-h-screen px-4 text-center'>
             <TransitionChild
-              as={Fragment}
               enter='ease-out duration-300'
               enterFrom='opacity-0'
               enterTo='opacity-100'
@@ -38,9 +37,13 @@ const Modal = () => {
               leaveFrom='opacity-100'
               leaveTo='opacity-0'
             >
-              <DialogPanel className='fixed, inset-0' />
+              <DialogPanel className='fixed inset-0' />
             </TransitionChild>
-            <span className='inline-block h-screen align-middle aria-hidden:' />
+
+            <span
+              className='inline-block h-screen align-middle'
+              aria-hidden='true'
+            />
 
             <TransitionChild
               as={Fragment}
@@ -62,8 +65,57 @@ const Modal = () => {
                         height={28}
                       />
                     </div>
+
+                    <Image
+                      src='/assets/icons/x-close.svg'
+                      alt='close'
+                      width={24}
+                      height={24}
+                      className='cursor-pointer'
+                      onClick={closeModal}
+                    />
                   </div>
+
+                  <h4 className='dialog-head_text'>
+                    Stay updated with product pricing alerts right in your
+                    inbox!
+                  </h4>
+
+                  <p className='text-sm text-gray-600 mt-2'>
+                    Never miss a bargain again with our timely alerts!
+                  </p>
                 </div>
+
+                <form className='flex flex-col mt-5' onSubmit={handleSubmit}>
+                  <label
+                    htmlFor='email'
+                    className='text-sm font-medium text-gray-700'
+                  >
+                    Email address
+                  </label>
+                  <div className='dialog-input_container'>
+                    <Image
+                      src='/assets/icons/mail.svg'
+                      alt='mail'
+                      width={18}
+                      height={18}
+                    />
+
+                    <input
+                      required
+                      type='email'
+                      id='email'
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      placeholder='Enter your email address'
+                      className='dialog-input'
+                    />
+                  </div>
+
+                  <button type='submit' className='dialog-btn'>
+                    {isSubmitting ? 'Submitting...' : 'Track'}
+                  </button>
+                </form>
               </div>
             </TransitionChild>
           </div>
