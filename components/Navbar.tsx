ebfdc03a44cc4e1994f303ef6309591a84584234
Arek <arek.adamowicz81@gmail.com>
2024-06-05 13:37:39 +0200

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,8 +15,8 @@ const Navbar = () => {
         <Link href='/' className='flex items-center gap-1'>
           <Image
             src='/assets/icons/logo.svg'
-            width={27}
-            height={27}
+            width={32}
+            height={32}
             alt='logo'
           />
 
